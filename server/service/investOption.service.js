985de1e5a6781ff9364c5cf38ac62f60f39a9dd0
phongnguyen9919 <phongnguyen9919@gmail.com>
2024-01-11 19:09:03 +0700

    fix vercel routes

--- server/service/investOption.service.js
+++ server/service/investOption.service.js
@@ -19,4 +19,11 @@ module.exports = {
       _id: new mongoose.Types.ObjectId(id),
     });
   },
+  totoalRevenue: async (invests) => {
+    revenue = 0;
+    for (const invest of invest) {
+      revenue += invest;
+    }
+    return revenue;
+  },
 };
